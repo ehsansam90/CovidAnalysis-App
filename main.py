6ccf5833c34fb3493d2985e519d14a4a01871acb
ehsansam90 <65367646+ehsansam90@users.noreply.github.com>
2022-12-24 18:42:33 -0500

    emojy

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ columns = ['state','actuals.newCases','actuals.cases','metrics.caseDensity','met
  'metrics.vaccinationsInitiatedRatio','metrics.vaccinationsCompletedRatio','actuals.deaths']
 states = {'AK': 'Alaska', 'AL': 'Alabama', 'AR': 'Arkansas', 'AS': 'American Samoa', 'AZ': 'Arizona', 'CA': 'California',  'CO': 'Colorado', 'CT': 'Connecticut', 'DC': 'District of Columbia',  'DE': 'Delaware', 'FL': 'Florida',  'GA': 'Georgia', 'GU': 'Guam', 'HI': 'Hawaii','IA': 'Iowa','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','MA': 'Massachusetts','MD': 'Maryland','ME': 'Maine','MI': 'Michigan','MN': 'Minnesota','MO': 'Missouri','MP': 'Northern Mariana Islands','MS': 'Mississippi','MT': 'Montana','NA': 'National','NC': 'North Carolina','ND': 'North Dakota','NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','PR': 'Puerto Rico','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VI': 'Virgin Islands','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
 
-st.title('Covid 19 Analysis across US  ')
+st.title('🦠Covid 19 Analysis across US 🦠 ')
 st.markdown("""
  **This web application would provide analysis on real time data related to Covid across all states located in United states**.\n
  1. Before beginning the analysis, it is important to specify the time range and location for the data collection. 
